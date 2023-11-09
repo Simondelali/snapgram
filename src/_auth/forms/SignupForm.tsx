855bc9e0fcd1f94b4074115718d441dde9deeeee
Simon Atiegar <atiegardelali@gmail.com>
2023-11-09 15:10:14 +0000

    fixes

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -36,12 +36,6 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     // const newUser = await createUserAccount(values) 
-    values = {
-      name: '',
-      username: '',
-      email: '',
-      password: '',
-    }
   }
   return (
     <Form {...form}>
